006df9f13f14476685a6805487ae52d7b5af6c5e
BbryanMatheusS <bryan14anosdetimao@gmail.com>
2022-07-04 23:14:38 -0300

    Inicio do meu projeto de teste da jussi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,122 @@
-import logo from './logo.svg';
+import './Reset.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="nav-bar">
+        <img src="" alt="" />
+        <ul>
+          <li>Nossas Soluçoes</li>
+          <li>Conheça  Jussi</li>
+          <li className='Search'>Barra de pesquisa</li>
+          <li>Login</li>
+          <li>icone do carrinho</li>
+        </ul>        
       </header>
+      <section className='hero-banner'>
+        <h1>Criamos lojas que vendem mais</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi nostrum neque, laudantium assumenda magni repellat quibusdam tempore. Eligendi animi sunt non aliquam a, quam cupiditate id fuga magnam laboriosam voluptatum.</p>
+        <button>Veja nossa soluçoes</button>
+        <div>
+          <img src="" alt="" />
+          <div>
+            <p></p>
+          </div>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <div>
+            <p></p>
+          </div>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <div>
+            <p></p>
+          </div>
+        </div>
+      </section>
+      <section className="logo_bar">
+        <h2>Nosssas principais lojas vtex</h2>
+        <p>--</p>
+        <ul>
+          <li>Brastemp</li>
+          <li>Compra Certa</li>
+          <li>Consul</li>
+          <li>The bar</li>
+        </ul>
+      </section>
+
+      <section className="our-soluctions">
+        <h2></h2>
+        <div>
+          <img src="" alt="" />
+          <h3>Nome do produto #1</h3>
+          <h4>Descriçao do produto #1</h4>
+          <p>feature 1</p>
+          <p>feature 2</p>
+          <p>feature 3</p>
+          <button>Ver Soluçao</button>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h3>Nome do produto #2</h3>
+          <h4>Descriçao do produto#2</h4>
+          <p>feature 1</p>
+          <p>feature 2</p>
+          <p>feature 3</p>
+          <button>Ver Soluçao</button>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h3>Nome do produto #3</h3>
+          <h4>Descriçao do produto #3</h4>
+          <p>feature 1</p>
+          <p>feature 2</p>
+          <p>feature 3</p>
+          <button>Ver Soluçao</button>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <h3>Nome do produto #4</h3>
+          <h4>Descriçao do produto #4</h4>
+          <p>feature 1</p>
+          <p>feature 2</p>
+          <p>feature 3</p>
+          <button>Ver Soluçao</button>
+        </div>
+      </section>
+
+      <section className="jussi-section">
+        <h2>olá.Somos a Jussi</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi corrupti voluptates eligendi. Placeat consectetur quibusdam non molestias ut aliquid facere ex labore, assumenda voluptates aspernatur pariatur optio sit suscipit ipsum?</p>
+        <button>Conheça a jussi</button>
+        <img src="" alt="" />
+      </section>
+
+      <section className="contact-info">
+        <h2>Essa loja foi construida usando uma das nossa soluçoes da plataforma VTEX. Tenha a sua</h2>
+        <h5>Entre em contato</h5>
+        <h3>Comercial@jussi.com.br</h3>
+      </section>
+
+      <section className="newletter">
+        <h2></h2>
+        <div>
+          <input type="email" name="" id="" />
+          <input type="submit" value="cadastrar" />
+        </div>
+      </section>
+      <footer>
+        <img src="" alt="" />
+        <ul>
+          <li>Facebook</li>
+          <li>Instagram</li>
+          <li>Linkedin</li>
+        </ul>
+      </footer>
+
     </div>
   );
 }
