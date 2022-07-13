02510c30bc9ecdc462f632d24ef3e529a2f73673
BbryanMatheusS <bryan14anosdetimao@gmail.com>
2022-07-13 10:43:13 -0300

    .

--- src/pages/Home/Footer/index.js
+++ src/pages/Home/Footer/index.js
@@ -12,11 +12,11 @@ const Footer = () => {
     return (
         <footer>
             <div className="footer__content content">
-                <a href="https://www.wpp.com/" target="_blank"><img src={logo} alt="WPP Company Logo" /></a>
+                <a href="https://www.wpp.com/" target="_blank" rel="noreferrer"><img src={logo} alt="WPP Company Logo" /></a>
                 <ul>
-                <li><a href="https://www.facebook.com/agencia.jussi" target="_blank"><img src={facebook} alt="Facebook" /></a></li>
-                <li><a href="https://www.instagram.com/jussi/" target="_blank"><img src={insta} alt="instagram" /></a></li>
-                <li><a href="https://www.linkedin.com/company/agencia-jussi/" target="_blank"><img src={linkedin} alt="linkedin" /></a></li>
+                <li><a href="https://www.facebook.com/agencia.jussi" target="_blank" rel="noreferrer"><img src={facebook} alt="Facebook" /></a></li>
+                <li><a href="https://www.instagram.com/jussi/" target="_blank" rel="noreferrer"><img src={insta} alt="instagram" /></a></li>
+                <li><a href="https://www.linkedin.com/company/agencia-jussi/" target="_blank" rel="noreferrer"><img src={linkedin} alt="linkedin" /></a></li>
                 </ul>
             </div>
         </footer>
